1ed7652b3c6e31f8e84c18031eb23a94ff79d736
eshikaa <36872799+eshikaa@users.noreply.github.com>
2018-04-16 17:03:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+
 app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+     res.sendFile(path.join(__dirname, 'ui', 'article-one.html));
 
 });
 app.get('/article-two',function(req,res)
