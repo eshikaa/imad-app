c6e2e1068f80b5dc1f927af7308fa39740ea6f4d
eshikaa <36872799+eshikaa@users.noreply.github.com>
2018-04-17 20:16:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,13 +22,14 @@ app.get('/article-one',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 
 });
-app.get('/article-two',function(req,res)
-{
-res.send('article 2 is served');    
+    
+app.get('/article-one',function(req,res){
+     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+
 });
-app.get('/article-three',function(req,res)
-{
-res.send('article 3 is served');    
+app.get('/article-one',function(req,res){
+     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+
 });
 function hash(input,salt)
 {
