d3f3a10b6a60d71431278cce914773a4c52d5a6c
eshikaa <36872799+eshikaa@users.noreply.github.com>
2018-04-17 21:11:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,79 @@ var path = require('path');
 var crypto=require('crypto');
 var app = express();
 app.use(morgan('combined'));
+var articles
+{
+   'article-one':(""),
+    'article-two':(""),
+    'article-three':("")
+};
+
+var article-one=
+{
+    title:'article-one';
+    date:'17 april 2018';
+    content:'   <p>is the first articelle.    jiocas</p>';
+                          
+};
+function createtemplate(data)
+{ 
+    var title=data.title;
+    var date=data.date;
+    var content=data.content;
+var htmltemplate={
+
+    <html>
+    <head>
+        <title>
+         $(title)
+            
+        </title>
+        <style>
+            .container
+            {
+                max-width:800px;
+                margin:0 auto;
+            }
+    </style>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+       <body>
+ <div class="container">
+         <div>
+            <a href="/">home</a>
+         </div>
+             <div>
+             $(date)
+              </div>
+                           <div>
+                                $(content)
+                          </div>
+</div>    
+         </body>
+      
+</html>
+;
+return htmltemplate;
+};
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -18,19 +91,21 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/articleName',function(req,res){
+     res.sendFile(createtemplate(articles[articleName]));
 
 });
+var articleName=req.params.articleName;
     
-app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+//app.get('/article-one',function(req,res){
+  //   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 
-});
-app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+//});
+//app.get('/article-one',function(req,res){
+  //   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 
-});
+//});
+
 function hash(input,salt)
 {
 var hashed=crypto.pbkdf2Sync(input,salt,10000,512,sha512);
