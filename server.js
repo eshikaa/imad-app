aba6834cf92424a048c3ca856cafeb2058202dfb
eshikaa <36872799+eshikaa@users.noreply.github.com>
2018-04-18 17:35:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,13 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString())
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 app.get('/articleName',function(req,res){
      res.sendFile(createtemplate(articles[articleName]));
